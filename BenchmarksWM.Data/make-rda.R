b3f8d8340a186106cb4d03656f090ed7d09e9617
Andy J. Wills <andy@willslab.co.uk>
2019-01-23 09:13:47 +0000

    Working checked R package with one dataset.

--- BenchmarksWM.Data/make-rda.R
+++ BenchmarksWM.Data/make-rda.R
@@ -2,6 +2,12 @@
 ## Author: Andy J. Wills
 ## Licence: GPL 2.0+
 library(tidyverse)
+
+## Unsworth & Engle (2006a)
+
+## Summary: Converted to long format. Test and length separated as
+## variables. Non-benchmark data excluded.
+
 rawd <- read_tsv("BM1.1.SetsizeAccuracy/Unsworth.Engle.Listlength.txt")
 un <- gather(rawd, worupc2:wmset5, key="comb", value="acc")
 un$size <- as.numeric(substr(un$comb, nchar(un$comb), nchar(un$comb)))
@@ -10,5 +16,5 @@ un <- un %>% select(subject, test, size, acc)
 un$test <- recode(un$test, worupc = "word", letupc = "letter", opupc = "operation", rspupc = "reading")
 un <- un %>% filter(test != "wmset") %>% filter(test != "stmset")
 un <- un %>% arrange(subject, test, size)
-unswoth06a <- un
-save(unsworth06a, file = "../data/unsworth06a.rda")
+unsworth06a <- un
+save(unsworth06a, file = "../pkg/data/unsworth06a.rda")
