0994f93991d6a0d1655b3d9c5fc8cb0cb4bf050b
Andy J. Wills <andy@willslab.co.uk>
2019-04-29 22:08:06 +0100

    Further work on WM1.1

--- build/make-rda.R
+++ build/make-rda.R
@@ -78,14 +78,17 @@ back  <- 1:5
 verhaeghen05  <- data.frame(back, acc)
 save(verhaeghen05, file = "../pkg/data/verhaeghen05.rda")
 
-### Oberauer & Kliegel (2006)
+### Oberauer & Kliegel (2001)
 fnam  <- paste0(pth, "/Oberauer.Kliegl.MU1.DAT")
+
+## Load Part 1
 colnames1 <- c("id", "setsize", "trial", "pt0", "pt1", "ptcat", "crit",
                "corrval1", "resp1", "correct1", "rt1", "corrval2", "resp2",
                "correct2", "rt2", "corrval3", "resp3", "correct3", "rt3",
                "corrval4", "resp4", "correct4", "rt4")
 mutaf1 <- read.table(fnam, header=F, fill=T, col.names=colnames1) 
 
+## Load Part 2
 colnames2 <- c("id", "setsize", "trial", "pt0", "pt1", "ptcat", "crit",
                "corrval1", "resp1", "correct1", "rt1", "corrval2", "resp2",
                "correct2", "rt2", "corrval3", "resp3", "correct3", "rt3",
@@ -93,32 +96,73 @@ colnames2 <- c("id", "setsize", "trial", "pt0", "pt1", "ptcat", "crit",
                "correct5", "rt5", "corrval6", "resp6", "correct6", "rt6")
 fnam  <- paste0(pth, "/Oberauer.Kliegl.MU2.DAT")
 mutaf2 <- read.table(fnam, header=F, fill=T, col.names=colnames2) 
+
+## Label parts
 mutaf1$exp = 1
 mutaf2$exp = 2
+
+## Remove setsize zero trials - not sure what these are...
 mutaf1 <- mutaf1[mutaf1$setsize>0,]
+
+## Filter to trials with maximum presentation duration (6s)
+## and to young adults (id < 30)
+mutaf1 <- mutaf1 %>% filter(pt0 > 5999) %>% filter(id < 30)
+mutaf2 <- mutaf2 %>% filter(pt0 > 5999) %>% filter(id < 30)
+
+## Select needed columns
+mutaf1 <- mutaf1 %>% select(id, exp, setsize, trial, correct1, correct2,
+                            correct3, correct4)
+
+## Convert to long format and arrange order
+mutaf1l <- mutaf1 %>%
+    gather(key = "subtrial", value = "correct", correct1:correct4) %>%
+    arrange(id, setsize, trial, subtrial)
+
+mutaf2 <- mutaf2 %>% select(id, exp, setsize, trial, correct1, correct2,
+                            correct3, correct4, correct5, correct6)
+
+
+#############################
+## Column numbers for columns whose labels include 'correct'
+## i.e. those that contain accuracy scores
 pcidx1 <- which(grepl("correct", colnames(mutaf1)))
 pcidx2 <- which(grepl("correct", colnames(mutaf2)))
+
+## Identify column containing setsize
 ssidx <- which(colnames(mutaf1)=="setsize")
+
+
+
+## Custom function to compute percent correct
 computePC <- function(x) {
   setsize <- as.numeric(x[1])
   pcvector <- as.numeric(x[2:(setsize+1)])
   return(mean(pcvector))}
 
+## Really inefficient way to work out %correct per trialk
 mutaf1$PC <- NULL
 for (j in 1:dim(mutaf1)[1]) {
   mutaf1[j,"PC"] <- computePC(mutaf1[j,c(ssidx, pcidx1)])
 }
-#mutaf1$pc <- apply(mutaf1[,c(ssidx, pcidx1)], MARGIN=2, FUN=computePC) # should do the same in theory, but does not work
+
 mutaf2$PC <- NULL
 for (j in 1:dim(mutaf2)[1]) {
   mutaf2[j,"PC"] <- computePC(mutaf2[j,c(ssidx, pcidx2)])
 }
+
+## Weird way to subset to needed columns
 mt1 <- mutaf1[, which(colnames(mutaf1) %in% c("id", "exp", "setsize", "pt0", "PC"))]
 mt2 <- mutaf2[, which(colnames(mutaf2) %in% c("id", "exp", "setsize", "pt0", "PC"))]
+
+## Combine
 mutaf <- rbind(mt1, mt2)
+
+## Select subj# < 30 (young adults)
+## ... and also for the maximal presentation time of 6 seconds.
 mt1.y.long <- subset(mt1, id < 30 & pt0 > 5999)
 mt2.y.long <- subset(mt2, id < 30 & pt0 > 5999)
 
+## Really inefficient way of getting accuracy by set size by subject
 nsubj <- length(unique(mt1.y.long$id))
 MUarray1 <- array(NA,dim=c(4,1,nsubj))
 for (ss in 1:4) {
@@ -126,6 +170,7 @@ for (ss in 1:4) {
     aggdat <- aggregate(PC ~ id, data=d, FUN=mean)
     MUarray1[ss,1,] <- aggdat$PC
 }
+
 nsubj <- length(unique(mt2.y.long$id))
 MUarray2 <- array(NA,dim=c(3,1,nsubj))
 for (ss in 4:6) {
